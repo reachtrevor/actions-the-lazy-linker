32f5aba804dc9e705292d08aa58b18f72209ef4e
Trevor Pierce <trevthewebdev@gmail.com>
2024-12-06 15:59:36 -0600

    quote fix?

--- src/jira-connector.js
+++ src/jira-connector.js
@@ -97,9 +97,9 @@ export class JiraConnector {
     let next = text;
 
     // replace first instance of {quote}, per line with >
-    next = text.replace(/^{quote}/, '> ');
+    next = text.replace(/^{quote}/gm, '> ');
     // replace all other instances of {quote} with empty string
-    next = text.replace(/{quote}$/, '');
+    next = text.replace(/{quote}$/gm, '');
 
     return next;
   }
